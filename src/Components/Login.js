28a39176caa2eda4dd3c9bf192d80f7c924db904
Akash Khatri <akashkhatri1001@gmail.com>
2022-01-16 16:45:10 +0530

    Add Login Page

---
+++ src/Components/Login.js
@@ -0,0 +1,119 @@
+import styled from "styled-components";
+
+function Login() {
+  return (
+    <Container>
+      <BgImage />
+      <Content>
+        <CTA>
+          <CTALogoOne
+            src="https://raw.githubusercontent.com/CleverProgrammers/cp-disney-plus-clone/master/public/images/cta-logo-one.svg"
+            alt=""
+          />
+          <SignUp>GET IT ALL THERE!</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
+          </Description>
+          <CTALogoTwo
+            src="https://raw.githubusercontent.com/CleverProgrammers/cp-disney-plus-clone/master/public/images/cta-logo-two.png"
+            alt=""
+          />
+        </CTA>
+      </Content>
+    </Container>
+  );
+}
+
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
+
+const BgImage = styled.div`
+  background-image: url("https://raw.githubusercontent.com/CleverProgrammers/cp-disney-plus-clone/master/public/images/login-background.jpg");
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+  max-width: 650px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  align-items: center;
+  text-align: center;
+
+  width: 100%;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  /* width: 100%; */
+`;
+
+const SignUp = styled.a`
+  font-weight: bold;
+  background-color: #0063e5;
+  color: white;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border-radius: 4px;
+  border: 1px solid transparent;
+
+  &:hover {
+    background-color: #0483ee;
+    cursor: pointer;
+  }
+`;
+
+const Description = styled.p`
+  color: hsl(0, 0%, 95.3%, 1);
+  font-size: 15px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
+`;
+
+export default Login;
